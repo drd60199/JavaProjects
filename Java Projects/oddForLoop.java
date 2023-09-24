3358163f73e541377b713024998cfa60ae52048b
Damien Dennis <dennis.damien95@outlook.com>
2023-09-24 04:42:29 -0500

    add oddForLoop

---
+++ Java Projects/oddForLoop.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[]args){
+        for(int number = 1; number <= 50; number++) {
+            if(number % 2 == 1) {
+            System.out.println(number);
+        }
+    }
+}
+}
+
+         
+      
+
+    
